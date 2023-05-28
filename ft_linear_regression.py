3feb6c20cc8520bf30dd7a28163e0b28e8ca9c88
grvelva <grvelva@student.42.fr>
2023-05-28 15:39:26 +0200

    started

---
+++ ft_linear_regression.py
@@ -0,0 +1,157 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy
+import math
+
+
+def _guard_(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return(func(*args, **kwargs))
+        except Exception as e:
+            print("exception", e) 
+            return None
+    return wrapper
+
+
+@_guard_
+def read_data(path):
+    """Reads data from path, returns pd.DataFame"""
+    return pd.read_csv(path)
+
+
+@_guard_
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+        x (ndarray): Shape (m,) Input to the model (Population of cities) 
+        y (ndarray): Shape (m,) Label (Actual profits for the cities)
+        w, b (scalar): Parameters of the model
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    m = x.shape[0] 
+    total_cost = 0
+    
+    for i in range(m):
+        total_cost += (w * x[i] + b - y[i]) ** 2
+
+    return total_cost / (2 * m)
+
+@_guard_
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray): Shape (m,) Input to the model (Population of cities) 
+      y (ndarray): Shape (m,) Label (Actual profits for the cities)
+      w, b (scalar): Parameters of the model  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+  
+    for i in range(m):
+        y_predict = w * x[i] + b
+        dj_dw += (y_predict - y[i]) * x[i]
+        dj_db += (y_predict - y[i])
+       
+    return dj_dw / m, dj_db / m
+
+@_guard_
+def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x :    (ndarray): Shape (m,)
+      y :    (ndarray): Shape (m,)
+      w_in, b_in : (scalar) Initial values of parameters of the model
+      cost_function: function to compute cost
+      gradient_function: function to compute the gradient
+      alpha : (float) Learning rate
+      num_iters : (int) number of iterations to run gradient descent
+    Returns
+      w : (ndarray): Shape (1,) Updated values of parameters of the model after
+          running gradient descent
+      b : (scalar)                Updated value of parameter of the model after
+          running gradient descent
+    """
+    
+    m = len(x)
+    
+    J_history = []
+    w_history = []
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_dw, dj_db = gradient_function(x, y, w, b )  
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            cost =  cost_function(x, y, w, b)
+            J_history.append(cost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            w_history.append(w)
+            print(f"Iteration {i:4}: Cost {float(J_history[-1]):8.2f}   ")
+        
+    return w, b, J_history, w_history #return w and J,w history for graphing
+
+if __name__ == "__main__":
+    data = read_data("./data.csv")
+    # Create a scatter plot of the data. To change the markers to red "x",
+    # we used the 'marker' and 'c' parameters
+    plt.scatter(data["km"], data["price"], marker='x', c='r') 
+
+    # Set the title
+    plt.title("Profits vs. Population per city")
+    # Set the y-axis label
+    plt.ylabel('Profit in $10,000')
+    # Set the x-axis label
+    plt.xlabel('Population of City in 10,000s')
+    plt.show()
+
+
+    # w, b, J_history, w_history = gradient_descent()
+    # PREDICT
+    w = 1
+    b = 1
+    m = data.shape[0]
+    predicted = np.zeros(m)
+
+    for i in range(m):
+        predicted[i] = w * data["km"][i] + b
+
+
+    plt.plot(data["km"], predicted, c = "b")
+
+    # Create a scatter plot of the data. 
+    plt.scatter(data["km"], data["price"], marker='x', c='r') 
+
+    # Set the title
+    plt.title("Profits vs. Population per city")
+    # Set the y-axis label
+    plt.ylabel('Profit in $10,000')
+    # Set the x-axis label
+    plt.xlabel('Population of City in 10,000s')
+    plt.show()
+
